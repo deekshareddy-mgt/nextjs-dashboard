f7203918251867204e59d7c939227c3e652fbe0a
deekshareddy-mgt <deeksha@mgtinsurance.com>
2025-04-25 14:45:36 -0600

    dashboard demo with navbar changes

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,7 +11,6 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 
 const links = [
-  
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
